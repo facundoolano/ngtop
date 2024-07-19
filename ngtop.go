8fbbd74276d265400d2b6617fdb017afeb1e1fd9
facundo <facundo.olano@gmail.com>
2024-07-19 12:53:44 -0300

    first stab at parsing the line

--- ngtop.go
+++ ngtop.go
@@ -1,13 +1,18 @@
 package main
 
 import (
+	"bufio"
 	"database/sql"
 	"log"
 	"os"
+	"path/filepath"
+	"regexp"
 
 	"github.com/alecthomas/kong"
 	_ "github.com/mattn/go-sqlite3"
 )
+
+const LOG_COMBINED_PATTERN = `(?P<remote_addr>\S+) - (?P<remote_user>\S+) \[(?P<time_local>.*?)\] "(?P<request>[^"]*)" (?P<status>\d{3}) (?P<body_bytes_sent>\d+) "(?P<http_referer>[^"]*)" "(?P<http_user_agent>[^"]*)"`
 
 // TODO add arg to parse log files
 var cli struct {
@@ -65,15 +70,54 @@ func dbInit(dbPath string, logFiles ...string) {
 	// without missing logs from partial/errored/missed files
 
 	for _, path := range logFiles {
+		// FIXME add zipped file support, don't rely on extension
+		if filepath.Ext(path) == ".gz" {
+			log.Printf("skipping zipped file %s", path)
+			continue
+		}
+
 		log.Printf("parsing %s", path)
-		srcFile, err := os.Open(path)
+		file, err := os.Open(path)
 		checkError(err)
-		defer srcFile.Close()
+		defer file.Close()
+
+		logPattern := regexp.MustCompile(LOG_COMBINED_PATTERN)
+		scanner := bufio.NewScanner(file)
+		for scanner.Scan() {
+			line := scanner.Text()
+			parsed := parseLogLine(logPattern, line)
+			if parsed == nil {
+				log.Printf("couldn't parse line %s", line)
+				continue
+			}
+
+			// TODO insert into table
+
+			log.Print(parsed)
+		}
+		checkError(scanner.Err())
+
 	}
 
 	// TODO accept more input files
 	// load all rows from input files
 
+}
+
+func parseLogLine(pattern *regexp.Regexp, logLine string) map[string]string {
+	match := pattern.FindStringSubmatch(logLine)
+	if match == nil {
+		return nil
+	}
+	result := make(map[string]string)
+	for i, name := range pattern.SubexpNames() {
+		if i != 0 && name != "" {
+			result[name] = match[i]
+		}
+	}
+	// TODO rename fields
+	// TODO add non raw
+	return result
 }
 
 func checkError(err error) {
