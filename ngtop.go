e606be32191f175a11259b9e99da2fa3151a6c4b
facundo <facundo.olano@gmail.com>
2024-07-19 12:11:13 -0300

    input log files

--- ngtop.go
+++ ngtop.go
@@ -2,70 +2,82 @@ package main
 
 import (
 	"database/sql"
-	"fmt"
+	"log"
+	"os"
+
 	"github.com/alecthomas/kong"
 	_ "github.com/mattn/go-sqlite3"
 )
 
 // TODO add arg to parse log files
 var cli struct {
+	Paths []string `arg:"" name:"path" help:"Paths to log files to ingest." type:"path"`
 }
 
 func main() {
-	ctx := kong.Parse(
+	kong.Parse(
 		&cli,
 		kong.UsageOnError(),
 		kong.HelpOptions{FlagsLast: true},
 		kong.Vars{"version": "ngtop v0.1.0"},
 	)
 
-	fmt.Println("Hello, world.")
-
 	// FIXME use a standard location by default
 	// FIXME allow override via cli arg or config
-	dbInit("./ngtop.db")
+	dbInit("./ngtop.db", cli.Paths...)
 
-	err := ctx.Run()
-	ctx.FatalIfErrorf(err)
+	// err := ctx.Run()
+	// ctx.FatalIfErrorf(err)
 }
 
 func dbInit(dbPath string, logFiles ...string) {
 	db, err := sql.Open("sqlite3", dbPath)
-	checkErr(err)
+	checkError(err)
 	defer db.Close()
 
 	sqlStmt := `
-	CREATE TABLE IF NOT EXISTS access_logs (
-		id 				INTEGER NOT NULL PRIMARY KEY,
+		CREATE TABLE IF NOT EXISTS access_logs (
+			id 				INTEGER NOT NULL PRIMARY KEY,
 
-		ip				TEXT,
-		time 			TIMESTAMP NOT NULL,
-		request_raw		TEXT,
-		status			INTEGER,
-		bytes_sent		INTEGER,
-		referrer 		TEXT,
-		user_agent_raw 	TEXT,
+			ip				TEXT,
+			time 			TIMESTAMP NOT NULL,
+			request_raw		TEXT NOT NULL,
+			status			INTEGER,
+			bytes_sent		INTEGER,
+			referrer 		TEXT,
+			user_agent_raw 	TEXT,
 
-		method			TEXT,
-		path			TEXT,
-		user_agent	 	TEXT,
+			method			TEXT,
+			path			TEXT,
+			user_agent	 	TEXT,
 
-		created TIMESTAMP DEFAULT CURRENT_TIMESTAMP
-	);
-`
+			created TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+
+			UNIQUE(ip, time, request_raw) ON CONFLICT REPLACE
+		);
+	`
 	_, err = db.Exec(sqlStmt)
-	checkErr(err)
+	checkError(err)
 
-	// open DB
-	// if not exist create tables
+	// TODO add indexes according to expected queries
+
+	// TODO figure out best approach to skip already loaded
+	// without missing logs from partial/errored/missed files
+
+	for _, path := range logFiles {
+		log.Printf("parsing %s", path)
+		srcFile, err := os.Open(path)
+		checkError(err)
+		defer srcFile.Close()
+	}
 
 	// TODO accept more input files
 	// load all rows from input files
 
 }
 
-func checkErr(err error) {
+func checkError(err error) {
 	if err != nil {
-		panic(err)
+		log.Panic(err)
 	}
 }
