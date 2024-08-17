98db853c2324a393d2865650fbe06f9f3e30ffe4
facundo <facundo.olano@gmail.com>
2024-08-16 21:36:53 -0300

    v0.4.4

--- main.go
+++ main.go
@@ -85,7 +85,7 @@ func querySpecFromCLI() (*kong.Context, *ngtop.RequestCountSpec) {
 		kong.Description("ngtop prints request counts from nginx access.logs based on a command-line query"),
 		kong.UsageOnError(),
 		kong.Vars{
-			"version": "ngtop v0.4.3",
+			"version": "ngtop v0.4.4",
 			"fields":  strings.Join(fieldNames, ","),
 		},
 	)
